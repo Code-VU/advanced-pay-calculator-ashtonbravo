185a81e565d8985582061e012a7ccff1b134a695
Ashton Bravo <Ashton.Bravo@vu.com>
2022-05-02 19:20:10 -0500

    test 18

--- payCalculator.py
+++ payCalculator.py
@@ -1,9 +1,9 @@
 def calculatePay():
     
     # This first line is provided for you
-    hrs = raw_input("Enter Hours:")
+    hrs = input("Enter Hours:")
     h = float(hrs)
-    rate = raw_input("Enter Rate:")
+    rate = input("Enter Rate:")
     rate = float(rate)
   
     #print (hrs,rate)
