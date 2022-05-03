83211802a896b4b6e6a7eb2c5f809502c53f1124
Ashton Bravo <Ashton.Bravo@vu.com>
2022-05-02 19:34:50 -0500

    test 25

--- payCalculator.py
+++ payCalculator.py
@@ -11,9 +11,13 @@ fr = float(sr)
 #print (fh, fr)
 if fh > 40: 
     print("Overtime")
+    reg = fr * fh
+    otp = (fh - 40.0) * (fr * 0.5)
+    print (reg,otp)
+    xp = reg + otp
 else:
     print("regular")
-xp = fh * fr
+    xp = fh * fr
 print("Pay:", xp)
 
 
